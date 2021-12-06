0ab96620dd348226b77b1e72820e6970cf8fb1f5
Loomin Arty <31379243+L1minus1@users.noreply.github.com>
2021-12-06 01:51:31 -0800

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -14,8 +14,8 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/L1minus1/dotfiles.git"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/L1minus1/dotfiles/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
